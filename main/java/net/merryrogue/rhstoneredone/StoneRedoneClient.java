51dda9a3d2a0979102b030a90179c64767428b1f
Merry Rogue <themerryrogue@gmail.com>
2022-08-06 10:34:51 -0700

    * Update "survive_explosion" condition in loot tables * Mud Brick Stairs at Crafting Table produce 6 stairs

---
+++ main/java/net/merryrogue/rhstoneredone/StoneRedoneClient.java
@@ -0,0 +1,10 @@
+package net.merryrogue.rhstoneredone;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StoneRedoneClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
